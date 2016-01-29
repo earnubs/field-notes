a583b24731dfc262d916aba6f6b8f45d0b592dc6
Stephen Stewart <stephen.stewart@carisenda.com>
2016-01-29 20:14:33 +0000

    minify and beautify

--- build.js
+++ build.js
@@ -1,16 +1,18 @@
+var beautify = require('js-beautify').html;
 var branch = require('metalsmith-branch');
 var collections = require('metalsmith-collections');
 var config = require('./config.json');
 var drafts = require('metalsmith-drafts');
 var layouts = require('metalsmith-layouts');
 var markdown = require('metalsmith-markdown');
-var metalsmith = require('metalsmith');
 var metallic = require('metalsmith-metallic');
+var metalsmith = require('metalsmith');
+var minify = require('html-minifier').minify;
 var moment = require('moment');
 var nunjucks = require('nunjucks');
+var path = require('path');
 var permalinks = require('metalsmith-permalinks');
 var typography = require('metalsmith-typography');
-var path = require('path');
 
 nunjucks.configure('./templates', {watch: false});
 
@@ -68,6 +70,40 @@ module.exports = function(callback) {
       moment: moment
     })
   )
+  .use(function(files, metalsmith, done) {
+    Object.keys(files).forEach(function(file) {
+      var content = files[file].contents.toString();
+      var min = minify(content, {
+        removeAttributeQuotes: true,
+        removeComments: true,
+        collapseBooleanAttributes: true,
+        removeRedundantAttributes: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeOptionalTags: true,
+        removeEmptyElements: true
+      });
+      var beaut = beautify(min, {
+        'indent_size': 2,
+        'indent_char': ' ',
+        'eol': '\n',
+        'indent_level': 0,
+        'indent_with_tabs': false,
+        'preserve_newlines': true,
+        'max_preserve_newlines': 0,
+        'brace_style': 'collapse',
+        'wrap_line_length': 0,
+        'wrap_attributes': 'auto',
+        'wrap_attributes_indent_size': 4,
+        'end_with_newline': true,
+        'extra_liners': 'head,body,pre,/html]'
+      });
+
+      files[file].contents = new Buffer(beaut);
+    });
+    done();
+  })
   .build(function(err) {
     if (err) {
       throw new Error(err);
