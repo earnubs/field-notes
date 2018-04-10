30defd32796243913ec4e999d717754ec1b9cd99
Stephen Stewart <stephen.stewart@carisenda.com>
2018-04-10 22:45:30 +0100

    Fix datetime class typo

--- clean_build.sh
+++ clean_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-docker build -t blog .
-docker run -dti -p 8080:8080 blog /bin/bash
+docker build --no-cache -t blog .
+docker run --rm -dti -p 8080:8080 blog /bin/bash
 open http://localhost:8080/
 CONTAINER_ID=$(docker ps -alq)
 if [ -d "./build" ]; then
