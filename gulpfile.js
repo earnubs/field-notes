2522b333b66138df08fc2e32604f536424434843
Stephen Stewart <stephen.stewart@carisenda.com>
2018-04-17 20:36:08 +0100

    v4 style

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,33 @@
 'use strict';
 
-var autoprefixer = require('autoprefixer');
-var concat = require('gulp-concat');
-var del = require('del');
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var csso = require('postcss-csso');
-var uglify = require('gulp-uglify');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const del = require('del');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const csso = require('postcss-csso');
+const metalsmith = require('./metalsmith.js');
 
-gulp.task('metalsmith', function (cb) {
-  let build = require('./metalsmith.js');
-  build(cb);
-  delete require.cache[require.resolve('./metalsmith.js')];
-});
+function clean() {
+  return del(['build']);
+}
 
-gulp.task('images', function() {
+function blog(cb) {
+  metalsmith(cb);
+}
+
+function images() {
   return gulp.src('./assets/images/**')
     .pipe(gulp.dest('build/static/img/'));
-});
+}
 
-gulp.task('fonts', function() {
+function fonts() {
   return gulp.src('./assets/fonts/**')
     .pipe(gulp.dest('build/static/font/'));
-});
+}
 
-gulp.task('styles', function () {
-  var processors = [
+function styles() {
+  const processors = [
     autoprefixer(),
     csso()
   ];
@@ -33,32 +35,43 @@ gulp.task('styles', function () {
     .pipe(postcss(processors))
     .pipe(concat('all.css'))
     .pipe(gulp.dest('build/static/css/'));
-});
+}
 
-gulp.task('scripts', function() {
-  return gulp.src('./assets/scripts/**')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/static/js/'));
-});
+const build = gulp.series(clean, gulp.parallel(blog, images, fonts, styles));
 
-gulp.task('manifest', function() {
-  return gulp.src('./manifest.json')
-    .pipe(gulp.dest('build/'));
-});
+gulp.task('default', build);
 
-gulp.task('sw', function() {
-  return gulp.src('./assets/sw/**')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/'));
-});
+//gulp.task('metalsmith', function (cb) {
+//  let build = require('./metalsmith.js');
+//  build(cb);
+//  delete require.cache[require.resolve('./metalsmith.js')];
+//});
+//
+//
+//gulp.task('scripts', function() {
+//  return gulp.src('./assets/scripts/**')
+//    .pipe(uglify())
+//    .pipe(gulp.dest('build/static/js/'));
+//});
+//
+//gulp.task('manifest', function() {
+//  return gulp.src('./manifest.json')
+//    .pipe(gulp.dest('build/'));
+//});
+//
+//gulp.task('sw', function() {
+//  return gulp.src('./assets/sw/**')
+//    .pipe(uglify())
+//    .pipe(gulp.dest('build/'));
+//});
+//
+//gulp.task('clean', function () {
+//  return del(['build']);
+//});
 
-gulp.task('clean', function () {
-  return del(['build']);
-});
+//gulp.task('watch', ['build'], function() {
+//  gulp.watch(['src/**/*', 'assets/**/*', 'templates/**/*'], ['build']);
+//});
 
-gulp.task('watch', ['build'], function() {
-  gulp.watch(['src/**/*', 'assets/**/*', 'templates/**/*'], ['build']);
-});
-
-gulp.task('build', ['metalsmith', 'scripts', 'styles', 'images', 'fonts', 'sw', 'manifest']);
-gulp.task('default', ['build']);
+//gulp.task('build', ['metalsmith', 'scripts', 'styles', 'images', 'fonts', 'sw', 'manifest']);
+//gulp.task('default', ['build']);
